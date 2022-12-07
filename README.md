a65e6bb074657807523a4f56c7a5ccd04c7ae60b
Doh1024 <dohyeongkim001024@gmail.com>
2022-12-07 14:24:23 -0600

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# what you might use this for
+- pinia makes the state management easier to use and make it less messy.
+pinia solves a lot of the quirks that vuex has.
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useMainStore} from "./store/useMain"
+import { storeToRefs } from 'pinia'
+
+const main = useMainStore();
+
+const{counter, name} = storeToRefs(main);
+</script>
+
+<template>
+
+  <h3>hello world</h3>
+  <h4>{{counter}}</h4>
+</template>
